3c658fa115f032460f9a2b22b29e293e2b3d34ff
ddd7975 <ddd7975@gmail.com>
2014-01-20 22:48:15 +0800

    didn't choose method

--- R/ChaoSpecies.R
+++ R/ChaoSpecies.R
@@ -1,7 +1,8 @@
 ChaoSpeciesOnline <-
 function(data, datatype = c("abundance", "incidence"), method = c("all", "Homogeneous", "Chao", "CE", "Jackknife"),
          k = 10, conf = 0.95, detail = TRUE){
-  if (method = Null)
+  if (sum(is.null(method)) != 0)
+    stop("Error: You haven't chosen the estimated method!")
   #method <- match.arg(method, several.ok=T)
   if (k != round(k) || k < 0) 
     stop("Error: The cutoff t to define less abundant species must be non-negative integer!")
@@ -24,13 +25,13 @@ function(data, datatype = c("abundance", "incidence"), method = c("all", "Homoge
       f <- function(i, data){length(data[which(data == i)])}
       if (f(1, data) == sum(data)){
         stop("Error: The information of data is not enough.")}
-      if (detail == T) basicAbun(data, k)
+      if (detail == T) basicAbuncat(data, k)
       SpeciesAbundance(data, method = method, k = k, conf = conf)
     } else {
       dat <- data[-1]; Q <- function(i, data){length(data[which(data == i)])}
       if (Q(1, dat) == sum(dat)){
         stop("Error: The information of data is not enough.")}
-      if (detail == T) basicInci(data, k)
+      if (detail == T) basicIncicat(data, k)
       SpeciesIncidence(data, method = method, k = k, conf = conf)  
     }
 }
