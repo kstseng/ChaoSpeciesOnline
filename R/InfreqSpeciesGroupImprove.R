418c1f8299bfb63790d27fddbd445cd7d5d142bf
ddd7975 <ddd7975@gmail.com>
2014-01-23 16:57:04 +0800

    delete name

--- R/InfreqSpeciesGroupImprove.R
+++ R/InfreqSpeciesGroupImprove.R
@@ -16,7 +16,7 @@ InfreqSpeciesGroupImprove <- function(data, k){
   x <- data[which(data != 0)]
   r <- c(1:k)
   Rare.Species.Group <- matrix(sapply(r, function(r)Q(r, x)), 1, k)
-  rownames(Rare.Species.Group) <- c("Infreq.Species.Group")
+  rownames(Rare.Species.Group) <- ""
   colnames(Rare.Species.Group) <- paste("Q", r, sep="")
   return(Rare.Species.Group)
 }
